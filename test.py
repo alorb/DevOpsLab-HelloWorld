bada492de92a3bbb00dd21a40302ea6a94b1a490
alexreis15 <alexreis15@yahoo.com.br>
2020-08-26 17:07:33 -0400

    test.py corrected

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Curso DevOps Essentials - Alorb \o/")
+        self.assertRegex(result.data.decode(), "Curso DevOpss Essentials - Alorb")
 
 
 if __name__ == "__main__":
